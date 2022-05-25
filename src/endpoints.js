d246e0fee6938f4db002af4d666a9744e9d8c14a
Leticia do Nascimento <ln9458143@gmail.com>
2022-05-25 14:07:25 -0300

    primeiro

---
+++ src/endpoints.js
@@ -0,0 +1,110 @@
+import { Router } from "express"
+import {dobro, CorPrimaria, ingresso, frequenciaCaracter, maiorNumero } from './services.js'
+
+const server = Router();
+
+server.get('/dobro/:numero', (req, resp) => {
+    let numero = Number(req.params.numero);
+    let x = dobro(numero);
+    resp.send({
+        dobro: x
+    })
+ })
+
+
+server.get('/somar', (req, resp) => {
+    const a = Number(req.query.a);
+    const b = Number(req.query.b);
+
+    const x = a + b;
+
+    resp.send({
+        somar: x
+    })
+})
+
+
+server.post('/somar', (req, resp) => {
+    try{
+    const { valores:{a: number1, b: number2} } = req.body;
+
+    const x = number1 + number2;
+
+    resp.send({
+        somar: x
+        })
+    } catch(err) {
+        resp.status(404).send({
+            erro:err.message
+        })
+    }  
+})
+
+
+server.get('/dia2/corprimaria/:cor', (req, resp) =>{
+
+    try {
+        const { cor } = req.params;
+        const primaria = CorPrimaria(cor);
+        resp.send({
+            primaria: primaria
+        });
+    } catch (err) {
+        resp.send({
+            erro:err.message
+        })
+        
+    }
+
+ })
+
+
+ server.post('/dia2/ingressocinema', (req, resp) => {
+     try {
+         const { qtdInteiras, qtdMeias, dia, nacionalidade } = req.body;
+         const total = ingresso(qtdInteiras, qtdMeias, dia, nacionalidade);
+         resp.send({
+             total: total
+         })
+     } catch (err) {
+         resp.send({
+             erro: err.message
+         })   
+     }
+ })
+
+
+ server.get('/dia2/freqcaractere/:texto/:caractere', (req, resp) => {
+     try {
+         const { texto, caractere } = req.params;
+         const freq = frequenciaCaracter(texto, caractere);
+         resp.send({
+             freq: freq
+         })
+     } catch (err) {
+         resp.send({
+             error: err.message
+         })
+         
+     }
+ })
+
+
+ server.post('/dia2/maiorNumero', (req, resp) => {
+     try {
+         const numeros = req.body;
+         const maior = maiorNumero(numeros);
+         resp.send({
+             maior: maior
+         })
+         
+     } catch (err) {
+         resp.send({
+             erro: err.message
+         })
+         
+     }
+ })
+
+
+export default server;
---
+++ src/services.js
@@ -0,0 +1,44 @@
+export function dobro(n){
+    return n * 2;
+}
+
+export function somar(a, b){
+    return a + b;
+}
+
+
+export function CorPrimaria(cor){
+    cor = cor.toLowerCase();
+    return cor == 'azul' || cor == 'vermelho' || cor =='amarelo'
+}
+
+export function ingresso(qtdInteiras, qtdMeias, dia, nacionalidade){
+    if(nacionalidade.toLowerCase() == 'brasileira')
+        return (qtdInteiras + qtdMeias) * 5;
+
+    else if(dia.toLowerCase() == 'quarta')
+        return (qtdInteiras +qtdMeias) * 14.25;
+
+    else
+        return (qtdInteiras * 28.5) + (qtdMeias * 14.25);
+}
+
+
+export function frequenciaCaracter(texto, caractere){
+    let qtd = 0;
+    for(let letra of texto) {
+        if(letra == caractere)
+        qtd++;
+    }
+        return qtd;
+}
+
+
+export function maiorNumero(numeros){
+    let maior = Number.MIN_VALUE;
+     for (let item of numeros) {
+         if(item > maior)
+            maior = item;
+    }
+    return maior;
+}
